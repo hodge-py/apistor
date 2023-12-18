c13ef62d1bdaa65c96d7098d3dd312732993ae2f
hodge-py <105604814+hodge-py@users.noreply.github.com>
2023-12-18 00:03:33 -0700

    register

---
+++ apistor.js
@@ -0,0 +1,23 @@
+/*
+fetch("http://localhost:3001/users", {
+  method: "POST",
+  body: JSON.stringify({
+    userId: 1,
+    title: "Fix my bugs",
+    completed: false
+  }),
+  headers: {
+    "Content-type": "application/json; charset=UTF-8"
+  }
+})
+  .then((response) => response.json())
+  .then((json) => console.log(json));
+
+  */
+
+
+  class Apistor {
+    
+
+
+  }
--- index.js
+++ index.js
@@ -4,7 +4,25 @@ const cors = require('cors');
 const helmet = require('helmet');
 const morgan = require('morgan');
 const app = express();
+const mysql = require('mysql2');
+
 // defining the Express app
+
+const connection = mysql.createConnection({
+  host: 'mysql-284bd9f9-khodge1-9a96.a.aivencloud.com',
+  user: 'avnadmin',
+  password: 'AVNS_uj8SPMvMDIqb7M7_NqU',
+  database: 'defaultdb',
+  port: 17203,
+});
+
+connection.query(
+  'SELECT * FROM `users`',
+  function(err, results, fields) {
+    console.log(results); // results contains rows returned by server
+    console.log(fields); // fields contains extra meta data about results, if available
+  }
+);
 
 // defining an array to work as the database (temporary solution)
 const ads = [
@@ -33,6 +51,28 @@ app.listen(3001, () => {
   console.log('listening on port 3001');
 });
 
-app.post('/users', (req, res) => {
-  res.send("hey")
+app.post('/register', (req, res) => {
+  code = authcode(30);
+
+  connection.query('INSERT INTO users (username, password, auth) VALUES (?,?,?)', [req.query.username, req.query.password, code],(error, results) => {
+     if (error) return res.json({ error: error });
+
+     });
+
+  res.send({Oauth: code});
 });
+
+
+
+
+function authcode(length){
+  let result = '';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const charactersLength = characters.length;
+    let counter = 0;
+    while (counter < length) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+      counter += 1;
+    }
+    return result;
+}
